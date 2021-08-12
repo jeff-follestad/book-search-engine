100a3ab3fa7fb0ac6ee07e26da46f629fc5774df
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-11 18:37:51 -0700

    Mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -32,6 +32,13 @@ const typeDefs = gql`
     link: String
   }
 
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput): User
+    removeBook(bookId: ID!): User
+  }
+
 `;
 
 module.exports = typeDefs;
